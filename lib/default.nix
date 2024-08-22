ab1a9127e871195b004a007878e73862b1d1be4b
yunfachi <yunfachi@gmail.com>
2024-08-22 15:40:52 +0300

    fix(lib/default): correctly pass config to lib

--- lib/default.nix
+++ lib/default.nix
@@ -5,6 +5,8 @@
   ...
 }: let
   inherit (import ./umport.nix {inherit lib;}) umport;
+
+  attrset = import ./attrset.nix {inherit lib;};
 in {
   configurations = {
     myconfigName ? "myconfig",
@@ -16,14 +18,17 @@ in {
     exclude ? [],
     recursive ? true,
     specialArgs ? {},
-    internalSystemExtraModule ? {nixpkgs.hostPlatform = "x86_64-linux";}, # just a plug; FIXME
-  }: let
+    extraModules ? [],
+    mkConfigurationsSystemExtraModule ? {nixpkgs.hostPlatform = "x86_64-linux";}, # just a plug; FIXME
+  } @ topArgs: let
     files = umport {inherit paths exclude recursive;};
-    apply = import ./apply.nix {inherit lib homeManagerUser isHomeManager myconfigName;};
-    attrset = import ./attrset.nix {inherit lib;};
 
-    denixLib = config: isHomeManager: let
-      apply = import ./apply.nix {inherit lib homeManagerUser isHomeManager myconfigName;};
+    mkApply = isHomeManager: import ./apply.nix {inherit lib homeManagerUser isHomeManager myconfigName;};
+    mkDenixLib = {
+      config,
+      isHomeManager,
+    }: let
+      apply = mkApply isHomeManager;
 
       host = import ./host.nix {inherit lib apply config myconfigName options;};
       module = import ./module.nix {inherit lib apply attrset config myconfigName;};
@@ -32,72 +37,104 @@ in {
     in
       host // module // options // rice;
 
-    system = extraModule: homeManagerSystem: isHomeManager: let
-      nixosSystem = lib.nixosSystem {
-        specialArgs = specialArgs // {${denixLibName} = denixLib config isHomeManager;};
-        modules =
-          [
-            home-manager.nixosModules.home-manager
-            extraModule
-          ]
-          ++ files;
-      };
-      homeSystem = home-manager.lib.homeManagerConfiguration {
-        pkgs = homeManagerNixpkgs.legacyPackages.${homeManagerSystem};
-        extraSpecialArgs = specialArgs // {${denixLibName} = denixLib config isHomeManager;};
-        modules = [extraModule] ++ files;
-      };
+    mkSystem = {
+      isHomeManager,
+      homeManagerSystem,
+      internalExtraModules ? (apply: []),
+    }: let
+      nixosSystem = let
+        apply = mkApply false;
+      in
+        lib.nixosSystem {
+          specialArgs =
+            specialArgs
+            // {
+              ${denixLibName} = mkDenixLib {
+                config = nixosSystem.config;
+                isHomeManager = false;
+              };
+            };
+          modules = (internalExtraModules apply) ++ extraModules ++ files ++ [home-manager.nixosModules.home-manager];
+        };
+      homeSystem = let
+        apply = mkApply true;
+      in
+        home-manager.lib.homeManagerConfiguration {
+          extraSpecialArgs =
+            specialArgs
+            // {
+              ${denixLibName} = mkDenixLib {
+                # FIXME: nixosSystem is used, not homeSystem, because homeManagerConfiguration causes infinite recursion (maybe I should create an issue in home-manager?)
+                config = nixosSystem.config;
+                isHomeManager = true;
+              };
+            };
+          pkgs = homeManagerNixpkgs.legacyPackages.${homeManagerSystem};
+          modules = (internalExtraModules apply) ++ extraModules ++ files;
+        };
     in
       if isHomeManager
       then homeSystem
       else nixosSystem;
 
-    mkHost = host: rice: let
+    mkHost = {
+      host,
+      rice ? null,
+    }: let
+      myconfig = system.config.${myconfigName};
+
       wrap = name: cfg: x:
         if builtins.typeOf x == "lambda"
-        then
-          x {
-            inherit name cfg;
-            myconfig = config.${myconfigName};
-          }
+        then x {inherit name cfg myconfig;}
         else x;
-      wrapHost = wrap host.name (config.${myconfigName}.hosts.${host.name});
+      wrapHost = wrap host.name (myconfig.hosts.${host.name});
+
+      system = mkSystem {
+        inherit isHomeManager;
+        inherit (host) homeManagerSystem;
+        internalExtraModules = apply: [
+          {config.${myconfigName} = {inherit rice host;};}
+          (apply.all
+            (wrapHost host.myconfig)
+            (wrapHost host.nixos)
+            (wrapHost host.home))
+          (lib.optionalAttrs (rice != null) (let
+            wrapRice = wrap rice.name (myconfig.rices.${rice.name});
+          in
+            apply.all
+            (wrapRice rice.myconfig)
+            (wrapRice rice.nixos)
+            (wrapRice rice.home)))
+        ];
+      };
     in
-      system {
-        config.${myconfigName} = {inherit rice host;};
+      system;
 
-        imports =
-          [
-            (apply.myconfig (wrapHost host.myconfig))
-            (apply.nixos (wrapHost host.nixos))
-            (apply.home (wrapHost host.home))
-          ]
-          ++ (lib.optionals (rice != null) (let
-            wrapRice = wrap rice.name (config.${myconfigName}.rices.${rice.name});
-          in [
-            (apply.myconfig (wrapRice rice.myconfig))
-            (apply.nixos (wrapRice rice.nixos))
-            (apply.home (wrapRice rice.home))
-          ]));
-      }
-      host.homeManagerSystem
-      isHomeManager;
+    mkConfigurations = let
+      system = mkSystem {
+        isHomeManager = false;
+        homeManagerSystem = "x86_64-linux"; # just a plug; FIXME
+        internalExtraModules = _: [mkConfigurationsSystemExtraModule];
+      };
 
-    inherit ((system internalSystemExtraModule "useless" false)) config;
-    inherit (config.${myconfigName}) hosts rices;
-  in
-    (lib.concatMapAttrs (riceName: rice:
-      lib.attrsets.mapAttrs' (hostName: host: {
-        name = "${hostName}-${riceName}";
-        value = mkHost host rice;
-      })
+      inherit (system.config.${myconfigName}) hosts rices;
+    in
+      (builtins.mapAttrs (_: host:
+        mkHost {
+          inherit host;
+          rice =
+            if host.rice == null
+            then null
+            else rices.${host.rice};
+        })
       hosts)
-    rices)
-    // (builtins.mapAttrs (_: host:
-      mkHost host (
-        if host.rice == null
-        then null
-        else rices.${host.rice}
-      ))
-    hosts);
+      // (lib.concatMapAttrs (riceName: rice:
+        lib.attrsets.mapAttrs' (hostName: host: {
+          name = "${hostName}-${riceName}";
+          value = mkHost {inherit host rice;};
+        })
+        hosts)
+      rices);
+  in
+    mkConfigurations;
 }
