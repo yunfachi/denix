c0ef3d99b31333a1d2f49b2a9afbdcb713e0dc95
yunfachi <yunfachi@gmail.com>
2025-06-12 09:05:36 +0300

    templates: fix inputs

--- templates/minimal/flake.nix
+++ templates/minimal/flake.nix
@@ -7,18 +7,19 @@
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     denix = {
       url = "github:yunfachi/denix";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
+      inputs.nix-darwin.follows = "nix-darwin";
     };
   };
 
-  outputs = {
-    denix,
-    nixpkgs,
-    ...
-  } @ inputs: let
+  outputs = {denix, ...} @ inputs: let
     mkConfigurations = moduleSystem:
       denix.lib.configurations {
         inherit moduleSystem;
