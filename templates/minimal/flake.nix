5810de575abde8533c19f4e3b387515e4fb11043
yunfachi <yunfachi@gmail.com>
2025-06-21 18:16:39 +0300

    templates: add commented `useHomeManagerModule = false;`

--- templates/minimal/flake.nix
+++ templates/minimal/flake.nix
@@ -23,6 +23,7 @@
     mkConfigurations = moduleSystem:
       denix.lib.configurations {
         inherit moduleSystem;
+        # useHomeManagerModule = false;
         homeManagerUser = "sjohn"; #!!! REPLACEME
 
         paths = [./hosts ./modules ./rices];
