b818c61db9f666f482112d14dd9890cc19300a47
yunfachi <yunfachi@gmail.com>
2025-06-24 09:44:54 +0300

    lib/extension: init

--- lib/configurations/default.nix
+++ lib/configurations/default.nix
@@ -7,183 +7,259 @@
   ...
 } @ args: let
   configurations = {
+    # Denix
     myconfigName ? "myconfig",
     denixLibName ? "delib",
-    nixpkgs ? args.nixpkgs,
-    home-manager ? args.home-manager,
-    nix-darwin ? args.nix-darwin,
-    homeManagerNixpkgs ? nixpkgs,
-    homeManagerUser ? null, # not default value!
     moduleSystem ? null, # TODO: remove the default value once the deprecated isHomeManager is removed
-    isHomeManager ? null, # TODO: DEPRECATED since 2025/05/05
+    extensions ? [],
+    # Umport
     paths ? [],
     exclude ? [],
     recursive ? true,
+    # System
     specialArgs ? {},
     extraModules ? [],
-    mkConfigurationsSystemExtraModule ? {nixpkgs.hostPlatform = "x86_64-linux";}, # just a plug; FIXME
+    # Inputs
+    nixpkgs ? args.nixpkgs,
+    home-manager ? args.home-manager,
+    nix-darwin ? args.nix-darwin,
+    # Home Manager
+    homeManagerNixpkgs ? nixpkgs,
+    homeManagerUser ? null, # not default value!
+    isHomeManager ? null, # TODO: DEPRECATED since 2025/05/05
+    # Dev
+    mkConfigurationsSystemExtraModule ? {
+      nixpkgs.hostPlatform = "x86_64-linux";
+    }, # just a plug; FIXME
   } @ topArgs:
     (
       if topArgs ? isHomeManager
       then builtins.trace "'delib.configurations :: isHomeManager' is deprecated, use 'delib.configurations :: moduleSystem' with values \"nixos\", \"home\", or \"darwin\" instead."
       else _: _
-    ) (let
-      moduleSystem =
-        topArgs.moduleSystem
-        or (
-          if isHomeManager == null
-          then builtins.abort "Please specify 'delib.configurations :: moduleSystem'. Valid values are \"nixos\", \"home\", or \"darwin\"."
-          else
-            (
-              if isHomeManager
-              then "home"
-              else "nixos"
-            )
-        );
+    )
+    (
+      let
+        moduleSystem =
+          topArgs.moduleSystem or (
+            if isHomeManager == null
+            then builtins.abort "Please specify 'delib.configurations :: moduleSystem'. Valid values are \"nixos\", \"home\", or \"darwin\"."
+            else
+              (
+                if isHomeManager
+                then "home"
+                else "nixos"
+              )
+          );
 
-      files = delib.umport {inherit paths exclude recursive;};
+        files = delib.umport {inherit paths exclude recursive;};
 
-      mkApply = moduleSystem: useHomeManagerModule: import ./apply.nix {inherit useHomeManagerModule homeManagerUser moduleSystem myconfigName;};
-      mkDenixLib = {
-        config,
-        moduleSystem,
-        useHomeManagerModule,
-        currentHostName ? null,
-      }:
-        delib.extend (delib: _: let
-          apply = mkApply moduleSystem useHomeManagerModule;
-          callLib = file: import file {inherit delib lib apply config myconfigName currentHostName;};
+        mkApply = moduleSystem: useHomeManagerModule:
+          import ./apply.nix {
+            inherit
+              useHomeManagerModule
+              homeManagerUser
+              moduleSystem
+              myconfigName
+              ;
+          };
+        mkDenixLib = {
+          config,
+          moduleSystem,
+          useHomeManagerModule,
+          currentHostName ? null,
+        }: let
+          extendedDelib = delib.extend (
+            final: prev: let
+              apply = mkApply moduleSystem useHomeManagerModule;
+              inherit (final) _callLib;
+            in {
+              _callLibArgs =
+                prev._callLibArgs
+                // {
+                  inherit
+                    apply
+                    config
+                    myconfigName
+                    currentHostName
+                    ;
+                };
+
+              inherit
+                (_callLib ./host.nix)
+                host
+                hostSubmoduleOptions
+                hostOption
+                hostsOption
+                hostNamesAssertions
+                ;
+              inherit (_callLib ./module.nix) module;
+              inherit
+                (_callLib ./rice.nix)
+                rice
+                riceSubmoduleOptions
+                riceOption
+                ricesOption
+                riceNamesAssertions
+                ;
+            }
+          );
         in
-          {
-            # not really needed
-            # inherit apply;
-          }
-          // callLib ./host.nix
-          // callLib ./module.nix
-          // callLib ./rice.nix);
+          lib.pipe extendedDelib (
+            builtins.map (extension: lib: lib.extend extension.libExtension) extensions
+          );
 
-      mkSystem = {
-        moduleSystem,
-        useHomeManagerModule,
-        homeManagerSystem,
-        currentHostName,
-        internalExtraModules ? (moduleSystem_: []),
-      }:
-        if !topArgs ? homeManagerUser && useHomeManagerModule
-        then abort "Please specify 'delib.configurations :: useHomeManagerModule'. Valid values are true and false."
-        else let
-          nixosSystem = nixpkgs.lib.nixosSystem {
-            specialArgs =
-              specialArgs
-              // {
-                ${denixLibName} = mkDenixLib {
-                  config = nixosSystem.config;
-                  moduleSystem = "nixos";
-                  inherit currentHostName useHomeManagerModule;
+        mkSystem = {
+          moduleSystem,
+          useHomeManagerModule,
+          homeManagerSystem,
+          currentHostName,
+          internalExtraModules ? (moduleSystem_: []),
+        }:
+          if !topArgs ? homeManagerUser && useHomeManagerModule
+          then abort "Please specify 'delib.configurations :: useHomeManagerModule'. Valid values are true and false."
+          else let
+            extensionsModules = builtins.concatMap (extension: extension.modules) extensions;
+            nixosSystem = nixpkgs.lib.nixosSystem {
+              specialArgs =
+                specialArgs
+                // {
+                  ${denixLibName} = mkDenixLib {
+                    config = nixosSystem.config;
+                    moduleSystem = "nixos";
+                    inherit currentHostName useHomeManagerModule;
+                  };
+                  inherit useHomeManagerModule; # otherwise it's impossible to make config.home-manager optional when not useHomeManagerModule.
                 };
-                inherit useHomeManagerModule; # otherwise it's impossible to make config.home-manager optional when not useHomeManagerModule.
-              };
-            modules = (internalExtraModules "nixos") ++ extraModules ++ files ++ (lib.optionals useHomeManagerModule [home-manager.nixosModules.home-manager]);
-          };
-          homeSystem = home-manager.lib.homeManagerConfiguration {
-            extraSpecialArgs =
-              specialArgs
-              // {
-                ${denixLibName} = mkDenixLib {
-                  config = homeSystem.config;
-                  moduleSystem = "home";
-                  inherit currentHostName useHomeManagerModule;
-                };
-                inherit useHomeManagerModule; # otherwise it's impossible to make config.home-manager optional when not useHomeManagerModule.
-              };
-            pkgs = homeManagerNixpkgs.legacyPackages.${homeManagerSystem};
-            modules = (internalExtraModules "home") ++ extraModules ++ files;
-          };
-          darwinSystem = nix-darwin.lib.darwinSystem {
-            specialArgs =
-              specialArgs
-              // {
-                ${denixLibName} = mkDenixLib {
-                  config = darwinSystem.config;
-                  moduleSystem = "darwin";
-                  inherit currentHostName useHomeManagerModule;
-                };
-                inherit useHomeManagerModule; # otherwise it's impossible to make config.home-manager optional when not useHomeManagerModule.
-              };
-            # FIXME: is this really necessary?
-            # pkgs = ...;
-            modules = (internalExtraModules "darwin") ++ extraModules ++ files ++ (lib.optionals useHomeManagerModule [home-manager.darwinModules.home-manager]);
-          };
-        in
-          {
-            nixos = nixosSystem;
-            home = homeSystem;
-            darwin = darwinSystem;
-          }
-          .${
-            moduleSystem
-          };
-
-      inherit
-        ({rices = {};}
-          // (mkSystem {
-            # TODO: maybe add moduleSystem = "empty", which "apply" would skip entirely*?
-            moduleSystem = "nixos";
-            useHomeManagerModule = true; # FIXME
-            homeManagerSystem = "x86_64-linux"; # just a plug; FIXME
-            currentHostName = null;
-            internalExtraModules = _: [mkConfigurationsSystemExtraModule];
-          })
-          .config
-          .${
-            myconfigName
-          })
-        hosts
-        rices
-        ;
-
-      mkHost = {
-        host,
-        rice ? null,
-      }: let
-        myconfig = system.config.${myconfigName};
-
-        system = mkSystem {
-          inherit moduleSystem;
-          inherit (host) useHomeManagerModule homeManagerSystem;
-          currentHostName = host.name;
-          internalExtraModules = moduleSystem: let
-            apply = mkApply moduleSystem host.useHomeManagerModule;
-          in
-            [
-              ({options, ...}: {config.${myconfigName} = {inherit host;} // lib.optionalAttrs (options.${myconfigName} ? rice) {inherit rice;};})
-            ]
-            ++ (lib.optionals (rice != null) (apply.listOfEverything rice.myconfig rice.nixos rice.home rice.darwin))
-            ++ builtins.concatMap (riceName: (apply.listOfEverything rices.${riceName}.myconfig rices.${riceName}.nixos rices.${riceName}.home) rices.${riceName}.darwin) (rice.inherits or []);
-        };
-      in
-        system;
-
-      configurations = let
-        mkHostAttrs = riceName: rice: hostName: host:
-          lib.optionalAttrs (!rice.inheritanceOnly or false) {
-            "${lib.optionalString (moduleSystem == "home") "${homeManagerUser}@"}${hostName}${lib.optionalString (riceName != null) "-${riceName}"}" = mkHost {
-              inherit host;
-              rice =
-                if rice == null
-                then
-                  (
-                    if (host.rice or null) == null
-                    then null
-                    else rices.${host.rice}
-                  )
-                else rice;
+              modules =
+                (internalExtraModules "nixos")
+                ++ extraModules
+                ++ files
+                ++ (lib.optionals useHomeManagerModule [home-manager.nixosModules.home-manager])
+                ++ extensionsModules;
             };
+            homeSystem = home-manager.lib.homeManagerConfiguration {
+              extraSpecialArgs =
+                specialArgs
+                // {
+                  ${denixLibName} = mkDenixLib {
+                    config = homeSystem.config;
+                    moduleSystem = "home";
+                    inherit currentHostName useHomeManagerModule;
+                  };
+                  inherit useHomeManagerModule; # otherwise it's impossible to make config.home-manager optional when not useHomeManagerModule.
+                };
+              pkgs = homeManagerNixpkgs.legacyPackages.${homeManagerSystem};
+              modules = (internalExtraModules "home") ++ extraModules ++ files ++ extensionsModules;
+            };
+            darwinSystem = nix-darwin.lib.darwinSystem {
+              specialArgs =
+                specialArgs
+                // {
+                  ${denixLibName} = mkDenixLib {
+                    config = darwinSystem.config;
+                    moduleSystem = "darwin";
+                    inherit currentHostName useHomeManagerModule;
+                  };
+                  inherit useHomeManagerModule; # otherwise it's impossible to make config.home-manager optional when not useHomeManagerModule.
+                };
+              # FIXME: is this really necessary?
+              # pkgs = ...;
+              modules =
+                (internalExtraModules "darwin")
+                ++ extraModules
+                ++ files
+                ++ (lib.optionals useHomeManagerModule [home-manager.darwinModules.home-manager])
+                ++ extensionsModules;
+            };
+          in
+            {
+              nixos = nixosSystem;
+              home = homeSystem;
+              darwin = darwinSystem;
+            }
+              .${
+              moduleSystem
+            };
+
+        inherit
+          (
+            {
+              rices = {};
+            }
+            // (mkSystem {
+              # TODO: maybe add moduleSystem = "empty", which "apply" would skip entirely*?
+              moduleSystem = "nixos";
+              useHomeManagerModule = true; # FIXME
+              homeManagerSystem = "x86_64-linux"; # just a plug; FIXME
+              currentHostName = null;
+              internalExtraModules = _: [mkConfigurationsSystemExtraModule];
+            }).config.${
+              myconfigName
+            }
+          )
+          hosts
+          rices
+          ;
+
+        mkHost = {
+          host,
+          rice ? null,
+        }: let
+          myconfig = system.config.${myconfigName};
+
+          system = mkSystem {
+            inherit moduleSystem;
+            inherit (host) useHomeManagerModule homeManagerSystem;
+            currentHostName = host.name;
+            internalExtraModules = moduleSystem: let
+              apply = mkApply moduleSystem host.useHomeManagerModule;
+            in
+              [
+                (
+                  {options, ...}: {
+                    config.${myconfigName} =
+                      {
+                        inherit host;
+                      }
+                      // lib.optionalAttrs (options.${myconfigName} ? rice) {inherit rice;};
+                  }
+                )
+              ]
+              ++ (lib.optionals (rice != null) (
+                apply.listOfEverything rice.myconfig rice.nixos rice.home rice.darwin
+              ))
+              ++ builtins.concatMap (
+                riceName:
+                  (apply.listOfEverything rices.${riceName}.myconfig rices.${riceName}.nixos rices.${riceName}.home)
+                  rices.${riceName}.darwin
+              ) (rice.inherits or []);
           };
+        in
+          system;
+
+        configurations = let
+          mkHostAttrs = riceName: rice: hostName: host:
+            lib.optionalAttrs (!rice.inheritanceOnly or false) {
+              "${lib.optionalString (moduleSystem == "home") "${homeManagerUser}@"}${hostName}${
+                lib.optionalString (riceName != null) "-${riceName}"
+              }" = mkHost {
+                inherit host;
+                rice =
+                  if rice == null
+                  then
+                    (
+                      if (host.rice or null) == null
+                      then null
+                      else rices.${host.rice}
+                    )
+                  else rice;
+              };
+            };
+        in
+          lib.concatMapAttrs (mkHostAttrs null null) hosts
+          // lib.concatMapAttrs (riceName: rice: lib.concatMapAttrs (mkHostAttrs riceName rice) hosts) rices;
       in
-        lib.concatMapAttrs (mkHostAttrs null null) hosts
-        // lib.concatMapAttrs (riceName: rice: lib.concatMapAttrs (mkHostAttrs riceName rice) hosts) rices;
-    in
-      configurations);
+        configurations
+    );
 in
   configurations
---
+++ lib/extension.nix
@@ -0,0 +1,168 @@
+{
+  delib,
+  lib,
+  ...
+}: let
+  pretty = value: (lib.generators.toPretty {} (
+    lib.generators.withRecursion {
+      depthLimit = 10;
+      throwOnDepthLimit = false;
+    }
+    value
+  ));
+  toExtensionWithConfig = f:
+    if lib.isFunction f
+    then
+      config: final: prev: let
+        fConfig = f prev;
+      in
+        if lib.isFunction fConfig
+        then let
+          fConfigPrev = f config prev;
+        in
+          if lib.isFunction fConfigPrev
+          then
+            # f is (config: final: prev: { ... })
+            f config final prev
+          else
+            # f is (config: prev: { ... })
+            fConfigPrev
+        else
+          # f is (config: { ... })
+          fConfig
+    else
+      # f is not a function; probably { ... }
+      config: final: prev:
+        f;
+in {
+  extension = {
+    # Meta
+    name,
+    description ? null,
+    maintainers ? [],
+    # Extension
+    config ? final: prev: {},
+    initialConfig ? null,
+    configOrder ? 0, # Used in mergeExtensions. Lower values mean earlier execution.
+    # Configuration
+    libExtension ? final: prev: {},
+    libExtensionOrder ? 0, # Used in mergeExtensions. Lower values mean earlier execution.
+    modules ? [],
+  }: let
+    _initialConfig = initialConfig;
+    f = _config: let
+      config = lib.toExtension _config;
+      initialConfig =
+        if _initialConfig == null || lib.isFunction _initialConfig
+        then _initialConfig
+        else _: _initialConfig;
+      fixedConfig = lib.fix (
+        lib.extends config (
+          if initialConfig != null
+          then initialConfig
+          else _: {}
+        )
+      );
+    in {
+      inherit
+        name
+        description
+        maintainers
+        initialConfig
+        configOrder
+        libExtensionOrder
+        ;
+
+      libExtension = (toExtensionWithConfig libExtension) fixedConfig;
+      modules =
+        if lib.isFunction modules
+        then modules fixedConfig
+        else modules;
+
+      config = fixedConfig;
+      __unfixConfig__ = config;
+      withConfig = configOverlay: f (lib.composeExtensions config configOverlay);
+      withFixedConfig = f;
+    };
+  in
+    f config;
+
+  callExtension = file: delib._callLib file // {_file = file;};
+
+  extensions = let
+    allExtensions = map delib.callExtension (delib.umport {paths = [./extensions];});
+    groupedByName = builtins.groupBy (extension: extension.name) allExtensions;
+  in
+    lib.mapAttrs delib.mergeExtensions groupedByName;
+
+  mergeExtensions = name: extensions: let
+    totalExtensions = builtins.length extensions;
+  in
+    if totalExtensions == 0
+    then delib.extension {inherit name;}
+    else if totalExtensions == 1
+    then builtins.removeAttrs (builtins.elemAt extensions 0) ["_file"]
+    else let
+      config = let
+        sorted = builtins.sort (q: p: q.configOrder < p.configOrder) extensions;
+        configs = builtins.map (x: x.__unfixConfig__) sorted;
+      in
+        lib.composeManyExtensions configs;
+    in
+      delib.extension {
+        inherit name;
+        description = let
+          withDescription = builtins.filter (extension: extension.description != null) extensions;
+          withUniqueDescription =
+            lib.foldl' (
+              acc: e:
+                if builtins.any (extension: e.description == extension.description) acc
+                then acc
+                else acc ++ [e]
+            ) []
+            withDescription;
+          totalUnique = builtins.length withUniqueDescription;
+        in
+          if totalUnique == 0
+          then null
+          else if totalUnique == 1
+          then (builtins.head withDescription).description
+          else
+            lib.warn (
+              "Denix extension with the name '${name}' has conflicting 'description' values:\n"
+              + (lib.concatMapStringsSep "\n" (
+                  extension: "- ${extension._file or extension.name}: ${pretty extension.description}"
+                )
+                withDescription)
+            )
+            null;
+
+        inherit config;
+        initialConfig = let
+          withInitialConfig = builtins.filter (extension: extension.initialConfig != null) extensions;
+          total = builtins.length withInitialConfig;
+        in
+          if total == 0
+          then null
+          else if total == 1
+          then (builtins.head withInitialConfig).initialConfig
+          else
+            lib.warn (
+              "Denix extension with the name '${name}' has conflicting 'initialConfig' values:\n"
+              + (lib.concatMapStringsSep "\n" (
+                  extension: "- ${extension._file or extension.name}: ${pretty extension.initialConfig}"
+                )
+                withInitialConfig)
+            )
+            null;
+
+        maintainers = lib.unique (builtins.concatMap (extension: extension.maintainers) extensions);
+
+        libExtension = let
+          sorted = builtins.sort (q: p: q.libExtensionOrder < p.libExtensionOrder) extensions;
+          libExtensions = config: builtins.map (extension: (extension.withFixedConfig config).libExtension) sorted;
+        in
+          config: lib.composeManyExtensions (libExtensions config);
+        modules = config: builtins.concatMap (extension: (extension.withFixedConfig config).modules) extensions;
+      };
+}
