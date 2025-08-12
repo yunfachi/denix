3f8ff7c06925185b1bbd5ab909976c62e9ebc3c6
yunfachi <yunfachi@gmail.com>
2025-08-12 10:16:57 +0300

    templates: use `delib.host :: system`

--- templates/minimal-no-rices/hosts/desktop/hardware.nix
+++ templates/minimal-no-rices/hosts/desktop/hardware.nix
@@ -2,13 +2,13 @@
 delib.host {
   name = "desktop";
 
+  system = "x86_64-linux"; # !!! REPLACEME
+
   # useHomeManagerModule = false;
-  homeManagerSystem = "x86_64-linux"; # !!! REPLACEME
   home.home.stateVersion = "24.05"; # !!! REPLACEME
 
   # If you're not using NixOS, you can remove this entire block.
   nixos = {
-    nixpkgs.hostPlatform = "x86_64-linux"; # !!! REPLACEME
     system.stateVersion = "24.05"; # !!! REPLACEME
 
     # nixos-generate-config --show-hardware-config
@@ -17,7 +17,6 @@ delib.host {
 
   # If you're not using Nix-Darwin, you can remove this entire block.
   darwin = {
-    nixpkgs.hostPlatform = "aarch64-darwin"; # !!! REPLACEME
     system.stateVersion = 6; # !!! REPLACEME
   };
 }
