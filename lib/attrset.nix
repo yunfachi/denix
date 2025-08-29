8fe5fa38f446cda93657e4e23943877b22c01fcb
shimeoki <shimeoki@gmail.com>
2025-08-29 19:53:31 +0300

    lib/attrset: fix missing splitStrPath

--- lib/attrset.nix
+++ lib/attrset.nix
@@ -4,9 +4,9 @@
 
   getAttrByStrPath =
     strPath: attrset: default:
-    lib.attrByPath (delib.splitStrPath strPath) default attrset;
+    lib.attrByPath (delib.attrset.splitStrPath strPath) default attrset;
 
-  setAttrByStrPath = strPath: value: lib.setAttrByPath (delib.splitStrPath strPath) value;
+  setAttrByStrPath = strPath: value: lib.setAttrByPath (delib.attrset.splitStrPath strPath) value;
 
   hasAttrs =
     attrs: attrset:
