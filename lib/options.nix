27c96f1a7e78959b84400c7488c0c16c600b52ff
shimeoki <shimeoki@gmail.com>
2025-08-29 22:39:21 +0300

    lib/options: add moduleOptions

--- lib/options.nix
+++ lib/options.nix
@@ -117,4 +117,6 @@ rec {
     default: { name, ... }: delib.attrset.setAttrByStrPath "${name}.enable" (boolOption default);
 
   singleCascadeEnableOption = args: delib.singleEnableOption args.parent.enable args;
+
+  moduleOptions = opts: args: delib.attrset.setAttrByStrPath args.name opts;
 }
